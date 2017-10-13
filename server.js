4843394e65266cc006bd53ad100dea65435e9c5c
Brody Macfarlane <macfarlanebrody@gmail.com>
2017-10-13 11:27:54 -0600

    server init setup

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var massive = require('massive')
+var cors = require('cors')
+require('dotenv').config()
+
+var app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const port = process.env.PORT || 3000
+
+
+
+app.listen(port, () => console.log(`listening on port ${port}`))
